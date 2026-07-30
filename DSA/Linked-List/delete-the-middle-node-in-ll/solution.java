a2eb6269259d97de811339685308b166e5b46b7b
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-07-30 13:10:02 +0530

    Add solution for Delete the middle node in LL - TUFHub

---
+++ DSA/Linked-List/delete-the-middle-node-in-ll/solution.java
@@ -0,0 +1,39 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slowPointerPrev = head;
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointerPrev = slowPointer;
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        slowPointerPrev.next=slowPointer.next;
+        slowPointer.next = null;
+        return head;
+
+    }
+}
