e27da9e2f83107d6cee6284fc753a69af82d9090
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-08-02 20:13:51 +0530

    Add solution for Detect a loop in LL - TUFHub

---
+++ DSA/Linked-List/detect-a-loop-in-ll/solution.java
@@ -0,0 +1,37 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet();
+        ListNode curr = head;
+        while(curr!=null){
+            if(curr == null) return false;
+            if(set.contains(curr)){
+                return true;
+            }
+            set.add(curr);
+            curr=curr.next;
+        }
+        return false;
+    }
+}
