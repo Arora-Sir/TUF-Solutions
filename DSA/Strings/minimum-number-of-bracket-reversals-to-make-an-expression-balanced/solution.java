f4110a0836b04f1fc5527f7112e0f7eb2a3fb794
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-06 22:03:13 +0530

    Sync Minimum number of bracket reversals to make an expression balanced [DSA] - TUFHub

--- DSA/Strings/minimum-number-of-bracket-reversals-to-make-an-expression-balanced/solution.java
+++ DSA/Strings/minimum-number-of-bracket-reversals-to-make-an-expression-balanced/solution.java
@@ -1,9 +1,12 @@
 class Solution {
     public int countRev(String s) {
         int n = s.length();
-        if(n%2 != 0) return -1;
+        if(n%2 != 0) return -1; //odd length can never be balanced
         int open = 0, close = 0;
-        for(int i=0; i < n; i++) {
+        // ")))(((" ; close:3 , open:3
+        // Two opening "((" or closing "))" needs 1 reversal each
+        // One closing & opening needs 1 reversal each
+        for(int i=0; i < n; i++) { 
             if(s.charAt(i) == '(') open++;
             else {
                 if(open > 0) open--;
