1bb393643e232fc879015c77a4c0bcbe47ccca9e
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-14 23:10:04 +0530

    Sync Fruit Into Baskets [DSA] - TUFHub

---
+++ DSA/Sliding-Window/fruit-into-baskets/Optimal.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int totalFruits(int[] fruits) {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    int left=0,ans=0;
+    for(int right=0;right<fruits.length;right++){
+      map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+      if(map.size()>2){ //if instead of while!
+        map.put(fruits[left],map.get(fruits[left])-1); //as contigious array we want and we can't pick more than 2 types of fruit we must empty the first basket we filled
+        if(map.get(fruits[left])==0) map.remove(fruits[left]);
+        left++;
+      }
+      else ans = Math.max(ans,right-left+1);
+    }
+    return ans;
+  }
+}
