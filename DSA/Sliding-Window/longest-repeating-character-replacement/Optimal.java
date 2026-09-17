f46600b11e03aacf837c423c8e0f86a6b4f34999
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-17 21:08:28 +0530

    Sync Longest Repeating Character Replacement [DSA] - TUFHub

--- DSA/Sliding-Window/longest-repeating-character-replacement/Optimal.java
+++ DSA/Sliding-Window/longest-repeating-character-replacement/Optimal.java
@@ -5,8 +5,8 @@ class Solution {
         int left=0;
         int[] freq=new int[26];
         for(int right=0;right<s.length();right++){  //O(N)
-            int currChar=++freq[s.charAt(right)-'A'];
-            maxFreq=Math.max(maxFreq,currChar);
+            int currCharFreq=++freq[s.charAt(right)-'A'];
+            maxFreq=Math.max(maxFreq,currCharFreq);
             while((right-left+1)-maxFreq>k){
                 freq[s.charAt(left)-'A']--;
                 left++;
