0abbb4eccf8daca6f817dbf0704fc8cab9434be6
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-22 02:41:43 +0530

    Sync Number of Substrings Containing All Three Characters [DSA] - TUFHub

---
+++ DSA/Sliding-Window/number-of-substrings-containing-all-three-characters/BruteBetter.java
@@ -0,0 +1,14 @@
+class Solution {    
+    public int numberOfSubstrings(String s) { //O(n^2)
+        int count=0; 
+        for(int i=0;i<s.length();i++){
+          int[] arrayOfAll=new int[3];
+          for(int j=i;j<s.length();j++){
+            arrayOfAll[s.charAt(j)-'a']=1; //as we have only a,b,c as charachters so it will be not an issue!
+            if(arrayOfAll[0]+arrayOfAll[1]+arrayOfAll[2]==3)
+              count++;
+          }
+        }
+        return count;
+    }
+}
