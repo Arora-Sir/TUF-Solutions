fd9e7439c174c09c887cbf58c8e2cd0af45ed381
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-23 23:16:32 +0530

    Delete SQL/Aggregation-And-Grouping/highest-non-repeating-number/solution.sql

--- SQL/Aggregation-And-Grouping/highest-non-repeating-number/solution.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-(   SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(*)=1
-    ORDER BY num DESC
-    LIMIT 1
-) AS num -- Added this outer Select to handle the NULL case, because if inner query returns NULL, then it will print null as nums as per question statement.
