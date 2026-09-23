bb9446bf5d3a2342d95017764f9ea79a26d696b0
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-23 16:38:08 +0530

    Sync Highest Order Placing Customer [SQL] (TUFHub)

--- SQL/Aggregation-And-Grouping/highest-order-placing-customer/solution.sql
+++ SQL/Aggregation-And-Grouping/highest-order-placing-customer/solution.sql
@@ -1,5 +1,5 @@
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-ORDER BY COUNT(customer_number) DESC
+ORDER BY COUNT(customer_number) DESC -- We can use COUNT(*) also as we have already grouped the cusromer id's
 LIMIT 1
