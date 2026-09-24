14ef02dda6ce94cc729dd4df4b67588b156d9243
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-09-24 23:24:10 +0530

    Sync Query Quality Analysis [SQL] (TUFHub)

--- SQL/Conditional-Logic-And-Calculations/query-quality-analysis/solution.sql
+++ SQL/Conditional-Logic-And-Calculations/query-quality-analysis/solution.sql
@@ -16,4 +16,4 @@ GROUP BY query_name
 -- Rules must to remember:
 -- Never write SELECT, FROM, or GROUP BY inside the column list. Once you write GROUP BY query_name at the bottom of the query, every function in your SELECT clause automatically calculates per group.
 -- Use AVG() instead of SUM() / COUNT(*) for averages and percentages.
--- COUNT(column) only ignores NULL. It never filters out 0 or false.
+-- COUNT(column) only ignores NULL. It never filters out 0 or false. So if we do COUNT(rating<3) it doesn't means it will count all with rating less than 3, it will actually is if the internal value is not null then return 1, and will return 100% instaed of the actual answer doesn't matter the rating is 1 or 5, as internal statement will return 1 or 0, and both not null so count increases everytime.
